37e38ffd8c28e83ec5bb9b2bee9accf7a9cc8475
rafaelsm <rafaelsm89@gmail.com>
2018-07-22 21:45:47 -0300

    more unit tests home presenter

--- app/src/test/java/br/com/rads/gistnator/home/HomePresenterTest.kt
+++ app/src/test/java/br/com/rads/gistnator/home/HomePresenterTest.kt
@@ -1,6 +1,8 @@
 package br.com.rads.gistnator.home
 
+import br.com.rads.gistnator.gist.Gist
 import br.com.rads.gistnator.gist.GistServiceApi
+import br.com.rads.gistnator.gist.response.FileContent
 import br.com.rads.gistnator.gist.response.GistsResponse
 import br.com.rads.gistnator.gist.response.Owner
 import br.com.rads.gistnator.rx.ScheduleProvider
@@ -11,8 +13,12 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.`when`
+import org.mockito.internal.stubbing.answers.AnswersWithDelay
+import org.mockito.internal.stubbing.answers.Returns
 
 class HomePresenterTest {
+
+    private val GIST = Gist("123", "owner", "name", "lang", "raw", "avatar")
 
     private lateinit var homePresenter: HomePresenter
     private val view = mock<HomeContract.View>()
@@ -60,6 +66,27 @@ class HomePresenterTest {
         verify(view, times(1)).addGistsToList(eq(listOf()))
     }
 
+    @Test
+    fun gistSelected() {
+        homePresenter.gistSelected(GIST)
+        verify(view, times(1)).openGist(GIST)
+    }
+
+    @Test
+    fun paginateSuccess() {
+        `when`(service.listGists(any(), any())).thenReturn(Observable.just(mockGistList()))
+        homePresenter.loadMoreGists()
+        verify(service, times(1)).listGists(any(), any())
+        verify(view, times(1)).addGistsToList(any())
+    }
+
+    @Test
+    fun paginateFail() {
+        `when`(service.listGists(any(), any())).thenReturn(Observable.error(Throwable("error mocked")))
+        homePresenter.loadMoreGists()
+        verify(service, times(1)).listGists(any(), any())
+    }
+
     @After
     fun tearDown() {
         verifyNoMoreInteractions(view, service)
@@ -69,10 +96,7 @@ class HomePresenterTest {
     private fun mockGistList(): List<GistsResponse> {
         return listOf(GistsResponse("", "", "", "", "", "", true,
                 Owner("", 1, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", false), "",
-                mapOf("files" to mapOf(
-                        "filename" to "",
-                        "language" to "",
-                        "raw_url" to "")),
+                mapOf("files" to FileContent(1, "", "", false, "")),
                 true, 1, "", "", "", "", "", ""))
     }
     //endregion
